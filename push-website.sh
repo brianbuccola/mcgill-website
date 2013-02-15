f0951ac258d4d7122f4a1bc024453fd8b907644f
Brian Buccola <brian.buccola@gmail.com>
2013-02-14 20:34:22 -0500

    initial commit of push-website.sh

---
+++ push-website.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+# vim:              set ft=sh tw=79 sw=4:
+# file:             push-website.sh
+# author:           Brian Buccola
+#
+# description:      Push website changes onto McGill server.
+
+SRC="$HOME/website"
+DEST="brian.buccola@people.linguistics.mcgill.ca:/home/bbucco/public_html"
+EXCL="$SRC/.push-website/exclude-list"
+LOG="$SRC/.push-website/log"
+
+rsync \
+    -avhhh -e "ssh -i $HOME/.ssh/id_rsa.mcgill" \
+    --exclude-from=$EXCL \
+    --log-file=$LOG \
+    $SRC/ $DEST/

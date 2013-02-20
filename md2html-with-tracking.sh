32328e3a6d090f085e32306729af7e40790cf1fb
Brian Buccola <brian.buccola@gmail.com>
2013-02-20 11:48:18 -0500

    added body and footer vars, pg_title and author vars, file_check function

--- md2html-with-tracking.sh
+++ md2html-with-tracking.sh
@@ -8,26 +8,34 @@
 #                   also adding Google Analytics tracking to <a href...>
 #                   elements.
 
+PG_TITLE="Brian Buccola"
+AUTHOR="Brian Buccola"
+
 IN_FILE="./index.markdown"
 OUT_FILE="./index.html"
+
 HEADER="./header.html"
+BODY="./body.html"
+FOOTER="./footer.html"
 
 ONCLICK_PDF="_gaq.push(['_trackEvent','Download','PDF',this.href]);"
 ONCLICK_TEX="_gaq.push(['_trackEvent','Download','TEX',this.href]);"
 
-if [[ ! -f "$IN_FILE" ]]; then
-    echo "Error: cannot find input markdown file."
-    echo "Check that $IN_FILE exists."
-    exit 1
-fi
+file_check() {
+    if [[ ! -f "$1" ]]; then
+        echo "Error: cannot find file ${1}."
+        exit 1
+    fi
+}
 
-if [[ ! -f "$HEADER" ]]; then
-    echo "Error: cannot find header file."
-    echo "Check that $HEADER exists."
-    exit 1
-fi
+file_check "$IN_FILE"
+file_check "$HEADER"
+file_check "$BODY"
+file_check "$FOOTER"
 
-pandoc -S -s -H "$HEADER" -f markdown -t html5 "$IN_FILE"     |\
-    sed "s|<a[^>]*\.pdf[^>]*|& onclick=\"$ONCLICK_PDF\"|g"    |\
-    sed "s|<a[^>]*\.tex[^>]*|& onclick=\"$ONCLICK_TEX\"|g"     \
+pandoc -S -H "$HEADER" -B "$BODY" -A "$FOOTER"                  \
+    -V pagetitle="$PG_TITLE" -V author-meta="$AUTHOR"           \
+    --mathjax -f markdown -t html5 "$IN_FILE"               |   \
+    sed "s|<a[^>]*\.pdf[^>]*|& onclick=\"$ONCLICK_PDF\"|g"  |   \
+    sed "s|<a[^>]*\.tex[^>]*|& onclick=\"$ONCLICK_TEX\"|g"      \
     > $OUT_FILE

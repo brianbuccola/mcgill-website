38c15129f2fe87b52dc40a955d68246412932cf6
Brian Buccola <brian.buccola@gmail.com>
2013-07-07 10:59:32 -0400

    added navigation bar; split index content into several pages

--- md2html-with-tracking.sh
+++ md2html-with-tracking.sh
@@ -7,39 +7,48 @@
 # description:      Coverts index.markdown into index.html using pandoc, while
 #                   also adding Google Analytics tracking to <a href...> tags.
 
+# Global variables.
 pg_title="Brian Buccola"
 author="Brian Buccola"
-
-in_file="index.markdown"
-out_file="index.html"
-
 css="style.css"
 head="head.html"
 before="before-body.html"
 after="after-body.html"
-
 onclick_pdf="_gaq.push(['_trackEvent','Download','PDF',this.href]);"
 onclick_tex="_gaq.push(['_trackEvent','Download','TEX',this.href]);"
+
+# in_file=$1
+# out_file="$(basename "$1" ".markdown").html"
 
 # Check if given file exists.
 file_check() {
     if [[ ! -f "$1" ]]; then
-        echo "Error: cannot find file ${1}."
+        echo "Error: cannot find file $1."
         exit 1
     fi
 }
 
-# Files to check.
-files=("$in_file" "$css" "$head" "$before" "$after")
-
-for f in "${files[@]}"; do
-    file_check "$f"
-done
-
 # Convert markdown to html5, and add onclick text to link tags.
-pandoc -S -c "$css" -H "$head" -B "$before" -A "$after"         \
-    -V pagetitle="$pg_title" -V author-meta="$author"           \
-    --mathjax -f markdown -t html5 "$in_file"               |   \
-    sed "s|<a[^>]*\.pdf[^>]*|& onclick=\"$onclick_pdf\"|g"  |   \
-    sed "s|<a[^>]*\.tex[^>]*|& onclick=\"$onclick_tex\"|g"      \
-    > $out_file
+md2html_with_tracking() {
+    local in_file=$1
+    local out_file="$(basename "$1" ".markdown").html"
+
+    # Files to check.
+    local files=("$in_file" "$css" "$head" "$before" "$after")
+
+    for f in "${files[@]}"; do
+        file_check "$f"
+    done
+
+    pandoc -S -c "$css" -H "$head" -B "$before" -A "$after"         \
+        -V pagetitle="$pg_title" -V author-meta="$author"           \
+        --mathjax -f markdown -t html5 "$in_file"               |   \
+        sed "s|<a[^>]*\.pdf[^>]*|& onclick=\"$onclick_pdf\"|g"  |   \
+        sed "s|<a[^>]*\.tex[^>]*|& onclick=\"$onclick_tex\"|g"      \
+        > $out_file
+}
+
+# Convert all markdown files in current directory to html5.
+for f in *.markdown; do
+    md2html_with_tracking "$f"
+done
